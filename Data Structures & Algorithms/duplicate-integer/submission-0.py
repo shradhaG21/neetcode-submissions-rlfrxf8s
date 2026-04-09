4eaa1315fdc418baee6e39609ff4452ed20317b3
shradhaG21 <157619222+shradhaG21@users.noreply.github.com>
2026-04-08 23:31:23 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+      # nums.sort()# Sort the array first so duplicates are next to each other 
+      # for i in range (0, len(nums) - 1): #Do len(nums) - 1 because when we get to the next line we will already get to the last index 
+       # if (nums[i] == nums[i+1]): # Compare the value at the current index to the next index. This should work because the sort function has alrready grouped the same values next to each other. 
+         #   return True #If there are duplicates, return true
+       # return False #If not return false. 
+
+
+        seen = set()
+        for i in range (0, len(nums)): 
+            if nums[i] not in seen: 
+                seen.add(nums[i]) 
+            else: 
+                return True 
+        return False
+       
